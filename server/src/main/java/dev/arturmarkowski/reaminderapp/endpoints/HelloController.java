e21741e17c58b0dbb9a2d85a0ce08bdb519a6893
Artur Markowski <markowski.artur@outlook.com>
2021-03-03 12:28:37 +0100

    added idea files, deleted exlipse, adde next endpoint

--- server/src/main/java/dev/arturmarkowski/reaminderapp/endpoints/HelloController.java
+++ server/src/main/java/dev/arturmarkowski/reaminderapp/endpoints/HelloController.java
@@ -5,10 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping("/")
 	public String index() {
 		return "Greetings from Spring Boot!";
 	}
 
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
 }
